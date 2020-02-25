4d31d0f8e5f43188ba30bf0702c40604a55e8c1d
Rob Mathews <robbis@gmail.com>
2020-02-25 15:07:16 +0000

    added pick_up_passenger

--- bus.rb
+++ bus.rb
@@ -1,14 +1,21 @@
 class Bus
 
-  attr_reader :route_number, :destination
+  attr_reader :route_number, :destination, :passengers
 
   def initialize(route_number, destination)
     @route_number = route_number
     @destination = destination
+    @passengers = []
   end
 
   def drive_method
     return "Brum brum"
   end
-  
+
+  def pick_up_passenger( passenger )
+
+    return @passengers << passenger
+
+  end
+
 end
--- specs/bus_spec.rb
+++ specs/bus_spec.rb
@@ -2,6 +2,7 @@ require('minitest/autorun')
 require('minitest/reporters')
 
 require_relative('../bus')
+require_relative('../person')
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
@@ -10,6 +11,7 @@ class TestBus < MiniTest::Test
   def setup
     @bus1 = Bus.new(22, "Ocean Terminal")
     @bus2 = Bus.new(47, "Granton")
+    @person1 = Person.new("Michael", 29)
   end
 
   def test_bus_route
@@ -22,6 +24,15 @@ class TestBus < MiniTest::Test
 
   def test_bus_drive_method
     assert_equal("Brum brum", @bus1.drive_method)
+  end
+
+  def test_pick_up_passenger()
+      @bus1.pick_up_passenger( @person1)
+    assert_equal(1, @bus1.passengers().size())
+  end
+
+  def method_name
+
   end
 
 end
